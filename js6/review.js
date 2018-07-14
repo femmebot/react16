c96a1be73707e87f97f2eda35d5b4940da61d741
femmebot <femmebot@gmail.com>
2018-07-13 23:26:13 -0400

    destructuring

--- js6/review.js
+++ js6/review.js
@@ -311,12 +311,29 @@ console.log(filter(1, 2, 3));
 
 
 
-
-
-
 // *********************************************
->>>>>>> fc5b39bdf922a5c64dfc0e75fa218a9bf51d1524
 // Destructuring
+
+// Easily extract array elements or object properties and store them in variables
+
+// Array destructuring
+[a, b] = ['Hello', 'max'];
+console.log(a) // Hello
+console.log(b) // max
+
+// Object destructuring
+{name} = {name: 'Max', age: 28};
+console.log(name) // Max
+console.log(age) // undefined
+
+// example
+const numbers = [1, 2, 3];
+[num1, num2] = numbers;
+console.log(num1, num2);
+
+[num1, , num3] = numbers;
+console.log(num1, num3);
+
 
 
 
