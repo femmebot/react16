fc5b39bdf922a5c64dfc0e75fa218a9bf51d1524
femmebot <femmebot@gmail.com>
2018-07-13 20:22:02 -0400

    rest + spread operators

--- js6/review.js
+++ js6/review.js
@@ -44,7 +44,7 @@ let double = number => number * 2;
 
 console.log(double(2))
 
-//
+// *********************************************
 // Exports & Imports (Modules)
 
 // person.js
@@ -67,12 +67,12 @@ import { baseData } from 'utility.js'   // no default requires named export usin
 import { clean } from 'utility.js'
 
 
-//
+// *********************************************
 // Understanding classes
 
 class Human {
   constructor() {
-    this.gender = 'male'
+    this.gender = 'male';
   }
   printGender() {
     console.log(this.gender);
@@ -81,12 +81,12 @@ class Human {
 class Person extends Human {
   constructor() {
     super();
-    this.name = 'Max'
-    this.gender = 'female'
+    this.name = 'Max';
+    this.gender = 'female';
   }
 
   printMyName() {
-    console.log(this.name)
+    console.log(this.name);
   }
 }
 
@@ -97,20 +97,95 @@ person.printMyGender();
 
 
 
-//
+// *********************************************
 // Classes, Properties and Methods
 
+// ES6
+// Properties are like variables attached to classes/objects
+constructor () {
+  this.myProperty = 'value'
+}
+
+// ES7
+// you can assign value directly to Property
+myProperty = 'value'
+
+// ES6
+// Methods are like functions attached to classes/objects
+myMethod () {
+  // something goes here
+}
+
+// ES7
+myMethod = () => {
+  // something goes here
+}
+
+// rewriting previous example from Classes
+class Human {
+  gender = 'male';
+  printGender = () => {
+    console.log(this.gender);
+  }
+}
+class Person extends Human {
+  name = 'Max'
+  gender = 'female'
+
+  printMyName = () => {
+    console.log(this.name);
+  }
+}
+
+const person = new Person();
+person.printMyName();
+person.printMyGender();
 
 
-//
-// THe Spread and Rest Operator
+
+// *********************************************
+// The Spread and Rest Operator ...
+
+// Spread is used to split up array elements or object properties
+const newArray = [...oldArray, 1, 2];
+const newObject = {...oldObject, newProp: 5};
+
+// Rest is used to merge a list of function arguments into an array
+function sortArgs(...args){
+  return args.sort();
+}
+
+// Spread operator examples
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4
+console.log(newNumbers);
+
+const person = {
+  name: 'Max';
+}
+
+const newPerson = {
+  ...person,
+  age: 28;
+}
+console.log(newPerson);
+
+// Rest operator example
+const filter = (...args) => {
+  return args.filter(el => el === 1);
+}
+
+console.log(filter(1, 2, 3));
 
 
 
-//
+
+
+
+// *********************************************
 // Destructuring
 
 
 
-//
+// *********************************************
 // Reference and Primitive Types
