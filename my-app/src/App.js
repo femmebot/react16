0508cb2efcada43478f5c17239bfa6a042d53539
femmebot <femmebot@gmail.com>
2018-07-16 14:25:59 -0400

    + Person component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Welcome to React</h1>
+        <Person/>
       </div>
     );
+
+    // return(React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi! Let’s do this!!!')))
   }
 }
 
---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+  return <p>I’m a person!</p>
+};
+
+export default person;
