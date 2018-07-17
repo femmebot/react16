fe611c7d0d9cb2473395db2f1dec40d36f3dd094
femmebot <femmebot@gmail.com>
2018-07-17 17:57:42 -0400

    +clickHandler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Welcome to React</h1>
-        <Person name="Max"  age="28"/>
-        <Person name="Manu"  age="29">My hobbies: Racing</Person>
-        <Person name="Stephanie"  age="26"/>
-
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name}  age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}/>
       </div>
     );
 
