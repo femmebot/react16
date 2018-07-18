073a60b6963ec992d00d82fd2c17278b5257565e
femmebot <femmebot@gmail.com>
2018-07-18 02:04:50 -0400

    manipulating the state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,7 +12,14 @@ class App extends Component {
   }
 
   switchNameHandler = () => {
-    console.log('Was clicked!');
+    // console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: "Maximillian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 }
+      ]
+    })
   }
 
   render() {
