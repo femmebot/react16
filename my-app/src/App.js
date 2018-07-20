28b8df7af62654e8d468431c5d6ec912096dae2a
femmebot <femmebot@gmail.com>
2018-07-19 23:05:42 -0400

    passing method references between components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,31 +5,46 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28 },
+      { name: "Maximilian", age: 28 },
       { name: "Manu", age: 29 },
       { name: "Stephanie", age: 26 }
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
+
+    // use special setState method from Component to update DOM
     this.setState({
       persons: [
-        { name: "Maximillian", age: 28 },
+        { name: newName, age: 28 },
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 27 }
       ]
     })
   }
 
+  // bind method preferred over passing anonymous function for performance
+  // <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
+
+
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Welcome to React</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name}  age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}>My hobbies: Racing
+          </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
 
