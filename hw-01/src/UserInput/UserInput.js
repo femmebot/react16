bb7e3d3bf115f2ed00bfeb0debe557e86a5e5115
femmebot <femmebot@gmail.com>
2018-07-26 01:57:21 -0400

    +hw-01 init

---
+++ hw-01/src/UserInput/UserInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const userInput = () => {
+  return (
+    <p>This is the user input text.</p>
+  )
+};
+
+export default userInput;
---
+++ hw-01/src/UserOutput/UserOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const userOutput = () => {
+  return (
+    <p>This is the user output text </p>
+  )
+};
+
+export default userOutput;
