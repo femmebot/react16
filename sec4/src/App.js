cd0e48d668a39d17c147e346b4287df2552518be
femmebot <femmebot@gmail.com>
2018-08-02 18:03:09 -0400

    use state map to list person data

--- sec4/src/App.js
+++ sec4/src/App.js
@@ -57,18 +57,12 @@ class App extends Component {
     if (this.state.showPerson) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}/>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}>My hobbies: Racing
-            </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}/>
+          {this.state.persons.map(person => {
+              return <Person
+                name = {person.name}
+                age = {person.age}/>
+            })
+          }
         </div>
       );
     }
